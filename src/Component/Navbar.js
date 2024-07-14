9ff0f91a971730ecc5829959ec3aab38f5a1245e
Chanuka999 <chanukaranditha99@gmail.com>
2024-07-13 18:50:10 -0700

    new changes

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Logo from "../Assests/Logo.svg";
-
+import { BsCart2 } from 'react-icons/bs';
+import { HiOutlineBars3 } from 'react-icons/hi2';
+import {
+  Box,
+  Drawer,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/icons-material";
+ 
+import HomeIcon from "@mui/icons-material/Home"
+import InfoIcon from "@mui/icons-material/Info"
+import CommentRoundedIcon from "@mui/icons-material/CommentRounded"
+import PhoneCartRoundIcon from "@mui/icons-material/PhoneCartRounded"
+import ShoppingCartRoundedIcon from "@mui/icons-material/ShoppingCartRounded"
 const Navbar = () => {
+
+  const[openMenu,setOpenMenu] = useState(false)
   return (
     <div>Navbar</div>
   )
